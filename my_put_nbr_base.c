948bee12816613ad2c97678b139c5f4c148a9623
Pluenet-Killian <killian.pluenet@gmail.com>
2023-10-25 13:59:14 +0200

    test & base

--- my_put_nbr_base.c
+++ my_put_nbr_base.c
@@ -5,7 +5,23 @@
 ** my_put_nbr_base
 */
 
+#include "my.h"
+#include <stdio.h>
+
+int compute_rest(int nbr) {
+    int result = nbr / 16;
+    int rest = nbr - (result * 16);;
+}
+
 int my_put_nbr_base(int nbr, char const *base)
 {
-    return 0;
+    int first = 0;
+    int end = 0;
+    end = nbr % 10;
+    first = nbr / 10;
+    if (first != 0) {
+        my_put_nbr_base(first, base);
+    }
+    my_putchar(end + '0');
+    compute_rest(nbr);
 }
--- tests/my_put_nbr_base_tests.c
+++ tests/my_put_nbr_base_tests.c
@@ -5,9 +5,26 @@
 ** nbr test
 */
 
+#include "../my.h"
+
 #include <criterion/criterion.h>
+#include <criterion/redirect.h>
 
 Test(my_put_nbr_base, is_defined)
 {
     return;
+}
+
+Test(my_put_nbr_base, show_nbr)
+{
+    cr_redirect_stdout();
+    my_put_nbr_base(348, "hexa");
+    cr_assert_stdout_eq_str("348");
+}
+
+Test(my_put_nbr_base, show_rest)
+{
+    cr_redirect_stdout();
+    my_put_nbr_base(347547, "hexadecimal");
+    cr_assert_stdout_eq_str("11");
 }
