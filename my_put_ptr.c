d6d0133d7908127a185069f7a4834b108c4494b3
Gabriel15066 <gabriel.mchiri@epitech.eu>
2023-10-25 21:18:27 +0200

    adding my_put_ptr

---
+++ my_put_ptr.c
@@ -0,0 +1,14 @@
+
+/*
+** EPITECH PROJECT, 2023
+** my_put_ptr.c
+** File description:
+** Function to displays ptr
+*/
+
+void my_put_ptr(unsigned long int *ptr)
+{
+    my_putstr("0x");
+    my_put_hexa_unsi_long(ptr);
+    return;
+}
