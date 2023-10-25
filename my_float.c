8d12b34ef0ce767f363b6a53e08200f9c7730f1a
Pluenet-Killian <killian.pluenet@gmail.com>
2023-10-25 17:17:06 +0200

    f

---
+++ my_float.c
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2023
+** MY_FLOAT
+** File description:
+** my_float
+*/
+
+#include "my.h"
+#include <stdio.h>
+
+double my_float(double nbr)
+{
+    my_putchar('1');
+    return 0.0;
+}
