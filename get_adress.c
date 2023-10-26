3d01cc77682963397afe4b3bd4ea38f7c751ed8b
Gabriel15066 <gabriel.mchiri@epitech.eu>
2023-10-26 09:24:55 +0200

    get-adress

---
+++ get_adress.c
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2023
+** my_put_ptr.c
+** File description:
+** Function to displays ptr
+*/
+
+#include <stdlib.h>
+
+void my_put_ptr(unsigned long int *ptr);
+
+{
+    my_putstr("0x");
+    my_put_hexa_unsi_long(ptr);
+    return;
+}
